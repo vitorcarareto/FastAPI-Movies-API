da8667462b0ea52667b7e4ed552eae7c14faa9ff
vitorcarareto <vitor.carareto@gmail.com>
2021-04-15 12:44:21 -0300

    User

---
+++ models/user.py
@@ -0,0 +1,16 @@
+from enum import Enum
+from fastapi import Query
+from pydantic import BaseModel
+
+
+class Role(Enum):
+    admin: str = "admin"
+    personal: str = "personal"
+
+
+class User(BaseModel):
+    id: int = None
+    username: str
+    password: str
+    email: str = Query(..., regex=r"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$")
+    role: Role = Role.personal
--- routes/v1.py
+++ routes/v1.py
@@ -1,4 +1,5 @@
 from fastapi import Body, File, APIRouter
+from models.user import User
 
 app = APIRouter()
 
@@ -7,3 +8,11 @@ app = APIRouter()
 async def healthcheck():
     return {"message": f"{__name__}"}
 
+@app.post('/user')
+async def post_user(user: User):
+    return {"message": user}
+
+
+@app.get('/user/{user_id}')
+async def get_user(user_id: int):
+    return {"message": user_id}
