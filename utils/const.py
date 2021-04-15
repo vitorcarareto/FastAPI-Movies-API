295e9db1f330b89669aadc3d6e608cc98079f16b
vitorcarareto <vitor.carareto@gmail.com>
2021-04-14 22:06:58 -0300

    Database setup

---
+++ utils/const.py
@@ -0,0 +1,11 @@
+import os
+
+JWT_SECRET_KEY = "583a3ceddd97521c7c2a22300e1dafd35a86d5d263d0e6136a0fbe025574f5ee"  # openssl rand -hex 32
+JWT_ALGORITHM = "HS256"
+JWT_EXPIRATION_TIME_MINUTES = 24 * 60  # 1 day
+
+DB_HOST = os.environ.get("DB_HOST", "localhost")
+DB_PORT = os.environ.get("DB_PORT", "5432")
+DB_NAME = os.environ.get("DB_NAME", "app")
+DB_USER = os.environ.get("DB_USER", "appuser")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "superS3cretpassw0rd")
---
+++ utils/db.py
@@ -0,0 +1,35 @@
+import asyncio
+from databases import Database
+from utils.const import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
+
+async def connect_db():
+    db = Database(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+    await db.connect()
+    return db
+
+async def disconnect_db(db):
+    await db.disconnect()
+
+
+async def execute(query, is_many, values=None):
+    db = await connect_db()
+
+    if is_many:
+        await db.execute_many(query=query, values=values)
+    else:
+        await db.execute(query=query, values=values)
+
+    await disconnect_db(db)
+
+async def fetch(query, is_one, values):
+    db = await connect_db()
+
+    if is_one:
+        result = await db.fetch_one(query=query, values=values)
+        output = dict(result)
+    else:
+        result = await db.fetch_all(query=query, values=values)
+        output = [dict(row) for row in result]
+
+    await disconnect_db(db)
+    return output
